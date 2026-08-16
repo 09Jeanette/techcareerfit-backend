0162c9d59adfa705df60811ce812df6b2cea2ff1
09 Jeanette <jeanettekgabe@gmail.com>
2026-08-16 11:40:55 +0200

    fixed the cors error

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api import auth
 from app.api import users
@@ -29,6 +30,20 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title="TechCareerFit API"
+)
+
+# CORS
+origins = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
